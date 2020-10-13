4636bec2059b0d8b034208d0b239e216da0a53fb
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-10-13 16:16:06 -0700

    Finished task 1

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ console.log('its working');
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
-(a) Home Team name for 2014 world cup final
+(a) Home Team name for 2014 world cup final//write a filter for this specific data
 (b) Away Team name for 2014 world cup final
 (c) Home Team goals for 2014 world cup final
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
-
-
+const conTwentyFourteen=fifaData.filter(function(item){
+    return item['Year']===2014 && item['Stage']==='Final';
+})
+console.log(conTwentyFourteen);
+console.log(conTwentyFourteen[0]['Home Team Name']);
+console.log(conTwentyFourteen[0]['Away Team Name']);
+console.log(conTwentyFourteen[0]['Home Team Goals']);
+console.log(conTwentyFourteen[0]['Away Team Goals']);
+console.log(conTwentyFourteen[0]['Win conditions']);
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
 function getFinals(/* code here */) {
@@ -22,7 +29,7 @@ function getFinals(/* code here */) {
 };
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
-
+//when writing initial callback function, use placeholder parameter names, use actual functions as arguments
 function getYears(/* code here */) {
 
     /* code here */
